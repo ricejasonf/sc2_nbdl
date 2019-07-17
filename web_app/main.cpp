00a65858ef2fe98933a2cd480c9a47dc66072f7f
Jason Rice <ricejasonf@gmail.com>
2019-07-16 17:35:39 -0700

    Adds support for running with cppdock dev_cluster

--- web_app/main.cpp
+++ web_app/main.cpp
@@ -6,8 +6,15 @@
 int main() {
   nbdl::js::init();
 
+  // auto web_main = nbdl::js::get_element_by_id("web_main")
+  // ^ not working in debug mode for some reason
+  nbdl::js::val web_main{};
+  NBDL_JS_TRANSFORM(web_main, function() {
+    return document.getElementById('web_main');
+  });
+
   static auto context = sc2_nbdl::web::make_context(
-    nbdl::actor("web_main", nbdl::js::get_element_by_id("web_main"))
+    nbdl::actor("web_main", web_main)
   );
 
   // Events are driven by javascript so things
